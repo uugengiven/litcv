56cc543ae6cd21a2777b46ba456ad088b7d3eda4
John Lange <lange.john.m@gmail.com>
2020-10-11 20:58:12 -0400

    Fixed zindex stuff

--- js/orbits.js
+++ js/orbits.js
@@ -1,6 +1,6 @@
 const orbits = {};
 
-orbits.setSizes = (x, y, z) => {
+orbits.setSizes = (x, y, z, distance) => {
     var sizes = {};
     sizes.x = [];
     sizes.x[0] = '-=' + x;
@@ -20,12 +20,12 @@ orbits.setSizes = (x, y, z) => {
     sizes.z[2] = '+=' + z;
     sizes.z[3] = '+=' + z;
 
-    sizes.index = [];
-    var zmodifier = z * 100;
-    sizes.index[0] = 200 - 10 * zmodifier;
-    sizes.index[1] = 200 - 100 * zmodifier;
-    sizes.index[2] = 200 - 10 * zmodifier;
-    sizes.index[3] = 200 + 100 * zmodifier;
+    sizes.zindex = [];
+    sizes.zindex[0] = 700 - distance;
+    sizes.zindex[1] = 700 - distance;
+    sizes.zindex[2] = 1000 + distance;
+    sizes.zindex[3] = 1000 + distance;
+    console.log("distance: ", distance)
 
     sizes.shadow = [];
     sizes.shadow[0] = '8px 8px';
@@ -35,13 +35,15 @@ orbits.setSizes = (x, y, z) => {
 
     return sizes;
   };
-orbits.startAnimation = (tl, el, x, y, z) => {
-    var sizes = orbits.setSizes(x, y, z);
+orbits.startAnimation = (tl, el, x, y, z, distance) => {
+    var sizes = orbits.setSizes(x, y, z, distance);
 
     var item = document.querySelector(el);
-    item.style.zIndex = sizes.index[3];
+    var itemContainer = document.querySelector(el + "-container")
+    itemContainer.style.zIndex = sizes.zindex[3];
     item.style.x = sizes.x[3];
     item.style.y = sizes.y[3];
+    tl.set(itemContainer, {zIndex: sizes.zindex[3]});
 
     for (var i = 0; i < 4; i++) {
       var ease1 = 'power1.in';
@@ -51,18 +53,27 @@ orbits.startAnimation = (tl, el, x, y, z) => {
         ease2 = 'power1.in';
       }
       console.log(i, ease1, ease2);
-      tl.to(el, { x: sizes.x[i], ease: ease1 }, i * 10);
+      tl.to(item, { x: sizes.x[i], ease: ease1 }, i * 10);
       // tl.to(el, {y: sizes.y[i], scale: sizes.z[i], boxShadow:"7px 7px " + sizes.shadow[i] + " gray", zIndex: sizes.index[i], ease: ease2}, i * 10);
       tl.to(
-        el,
+        item,
         {
           y: sizes.y[i],
           scale: sizes.z[i],
-          zIndex: sizes.index[i],
           ease: ease2
         },
         i * 10
       );
+      tl.to(
+        itemContainer,
+        {
+          zIndex: sizes.zindex[i],
+          ease: ease2
+        },
+        i * 10
+      );
+
+      console.log("zindex: ", sizes.zindex[i])
     }
   };
 
@@ -70,7 +81,7 @@ orbits.startAnimation = (tl, el, x, y, z) => {
     var tl = gsap.timeline({ defaults: { duration: 10 }, repeat: -1 });
 
     // startAnimation(tl, "#one", 150, 25, .1);
-    orbits.startAnimation(tl, '#two', 180, 20, 0.1);
-    orbits.startAnimation(tl, '#three', 220, 30, 0.15);
-    orbits.startAnimation(tl, '#four', 400, 40, 0.2);
+    orbits.startAnimation(tl, '#two', 180, 20, 0.1, 30);
+    orbits.startAnimation(tl, '#three', 220, 30, 0.15, 40);
+    orbits.startAnimation(tl, '#four', 400, 40, 0.2, 50);
   };
