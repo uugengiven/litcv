10ce10f12044706b7e057a832cbe968b961a75ee
Laura Padilla <lauvpadilla@gmail.com>
2020-11-09 23:07:34 -0500

    add menu navigation animations

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,29 @@ const menuToggle = document.querySelector("a.menu-toggle");
 
 menuToggle.addEventListener("click", function () {
   bodyTag.classList.toggle("nav-open");
+
+  if (bodyTag.classList.contains("nav-open")) {
+    gsap.to(".menu-top", { rotation: 45, transformOrigin: "50% 50%", y: 6 });
+    gsap.to(".menu-bottom", {
+      rotation: -45,
+      transformOrigin: "50% 50%",
+      y: -6,
+    });
+
+    const links = document.querySelectorAll("nav a");
+    gsap.set(links, { opacity: 0 });
+
+    gsap.to(links, {
+      opacity: 1,
+      delay: 0.3,
+      stagger: 0.17,
+      ease: "sine.in",
+    });
+  } else {
+    gsap.to(".menu-top", { rotation: 0, y: 0 });
+    gsap.to(".menu-bottom", {
+      rotation: 0,
+      y: 0,
+    });
+  }
 });
