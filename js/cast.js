e969846a75db0a4c35a858810bba63e814ba3f62
Laura Padilla <lauvpadilla@gmail.com>
2020-11-10 22:44:05 -0500

    add credits scroll to cast page

---
+++ js/cast.js
@@ -0,0 +1,14 @@
+// function makeMarquee() {
+//   const castAndCrew = document.querySelector("div");
+
+//   // an array is a list of things
+//   const creditsText = new Array(100).fill(castAndCrew);
+
+//   // 1. we want to grab out marquee span form the html
+//   // 2. we want to set our repeating title as the content
+
+//   const credits = document.querySelector("div.creditsScroll div");
+//   credits.innerHTML = creditsText;
+// }
+
+// makeMarquee();
