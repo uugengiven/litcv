a45caa35b896a4424b0763221106377c1eba7e8e
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-04 16:20:08 -0500

    add: pointer events handling for main d3 graph and force-directed graph

--- js/custom_particles.js
+++ js/custom_particles.js
@@ -1,26 +1,26 @@
 const particles = () => {
-  tsParticles.load("tsparticles", {
+  tsParticles.load('tsparticles', {
     fpsLimit: 60,
     particles: {
       number: {
         value: 50,
         density: {
           enable: true,
-          value_area: 800,
-        },
+          value_area: 800
+        }
       },
       color: {
-        value: "#f5f5f5",
+        value: '#f5f5f5'
       },
       shape: {
-        type: "circle",
+        type: 'circle',
         stroke: {
           width: 0,
-          color: "#f5f5f5",
+          color: '#f5f5f5'
         },
         polygon: {
-          nb_sides: 5,
-        },
+          nb_sides: 5
+        }
       },
       opacity: {
         value: 0.5,
@@ -29,8 +29,8 @@ const particles = () => {
           enable: false,
           speed: 1,
           opacity_min: 0.1,
-          sync: false,
-        },
+          sync: false
+        }
       },
       size: {
         value: 2,
@@ -39,85 +39,85 @@ const particles = () => {
           enable: false,
           speed: 20,
           size_min: 0.1,
-          sync: false,
-        },
+          sync: false
+        }
       },
       line_linked: {
         enable: true,
         distance: 150,
         // color: '#945050',
         // color: '#783434',
-        color: "#ccc",
+        color: '#ccc',
         opacity: 0.25,
-        width: 1,
+        width: 1
       },
       move: {
         enable: true,
         speed: 1,
-        direction: "none",
+        direction: 'none',
         random: false,
         straight: false,
-        out_mode: "out",
+        out_mode: 'out',
         attract: {
           enable: false,
           rotateX: 600,
-          rotateY: 1200,
-        },
+          rotateY: 1200
+        }
       },
       life: {
         duration: {
           sync: false,
-          value: 3,
+          value: 3
         },
         count: 0,
         delay: {
           random: {
             enable: true,
-            minimumValue: 0.5,
+            minimumValue: 0.5
           },
-          value: 1,
-        },
-      },
+          value: 1
+        }
+      }
     },
     interactivity: {
-      detect_on: "canvas",
+      detect_on: 'canvas',
       events: {
         onhover: {
           enable: true,
-          mode: "grab",
+          mode: 'grab'
         },
         onclick: {
           enable: true,
-          mode: "push",
+          mode: 'push'
         },
-        resize: true,
+        resize: true
       },
       modes: {
         grab: {
           distance: 150,
           line_linked: {
-            opacity: 1,
-          },
+            opacity: 1
+          }
         },
         bubble: {
           distance: 400,
           size: 40,
           duration: 2,
           opacity: 0.8,
-          speed: 3,
+          speed: 3
         },
         repulse: {
           distance: 200,
-          duration: 0.4,
+          duration: 0.4
         },
         push: {
-          particles_nb: 4,
+          particles_nb: 4
         },
         remove: {
-          particles_nb: 2,
-        },
-      },
+          particles_nb: 2
+        }
+      }
     },
-    retina_detect: true,
+    retina_detect: true
   });
 };
